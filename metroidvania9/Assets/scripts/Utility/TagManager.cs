7ee53f6864081e28dffa62cd7bfcca4c8048ee25
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2020-08-15 20:06:34 +0300

    Simple Movement added

---
+++ metroidvania9/Assets/scripts/Utility/TagManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis 
+{
+    public const string HORIZONTAL_AXIS = "Horizontal";
+    public const string VERTICAL_AXIS = "Vertical";
+}
+public class Keys
+{
+    public const KeyCode JUMP = KeyCode.Space; 
+}
---
+++ metroidvania9/Assets/scripts/character/CharacterMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Camera cam;
+    public float InputX;
+    public float InputZ;
+    public Vector3 desiredMoveDirection;
+    public float desiredRotationSpeed;
+    public float speed;
+    public Vector3 moveVector;
+    public float gravity = 14.0f;
+    public float jumpForce = 10.0f;
+    public bool isGrounded;
+    private float verticalVelocity;
+
+
+    void Update()
+    {
+        PlayerMovementAndRotation();
+
+        this.isGrounded = controller.isGrounded;
+
+        if(this.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if(Input.GetKeyDown(Keys.JUMP))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime; 
+        }
+        moveVector = new Vector3(0, verticalVelocity, 0).normalized + desiredMoveDirection;
+        controller.Move(moveVector.normalized * speed * Time.deltaTime);
+    }
+    void PlayerMovementAndRotation()
+    {
+        InputX = Input.GetAxis(Axis.HORIZONTAL_AXIS);
+        InputZ = Input.GetAxis(Axis.VERTICAL_AXIS);
+
+        Vector3 forward = cam.transform.forward;
+        Vector3 right = cam.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        desiredMoveDirection = forward * InputZ + right * InputX;
+
+        if(desiredMoveDirection.sqrMagnitude != 0)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desiredRotationSpeed); 
+    }
+
+    void InputMagnitude()
+    {
+        PlayerMovementAndRotation();
+        //
+        //Set animations
+        //
+    }
+}
