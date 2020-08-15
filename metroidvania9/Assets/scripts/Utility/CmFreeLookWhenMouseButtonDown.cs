2a96a2035df2dbe03506fa7cf07523d5075a9a4e
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2020-08-15 23:03:04 +0300

    camera contol can now be inverted

--- metroidvania9/Assets/scripts/Utility/CmFreeLookWhenMouseButtonDown.cs
+++ metroidvania9/Assets/scripts/Utility/CmFreeLookWhenMouseButtonDown.cs
@@ -10,21 +10,60 @@ public class CmFreeLookWhenMouseButtonDown : MonoBehaviour
     void Start(){
         CinemachineCore.GetInputAxis = GetAxisCustom;
     }
+    public bool isInverted = false;
     public float GetAxisCustom(string axisName){
-        if(axisName == "Mouse X"){
-            if (Input.GetMouseButton(1)){
-                return UnityEngine.Input.GetAxis("Mouse X");
-            } else{
-                return 0;
+        if( !isInverted )
+        {
+            if(axisName == "Mouse X")
+            {
+                if (Input.GetMouseButton(1))
+                {
+                    return -UnityEngine.Input.GetAxis("Mouse X");
+                } 
+                else
+                {
+                    return 0;
+                }
             }
-        }
-        else if (axisName == "Mouse Y"){
-            if (Input.GetMouseButton(1)){
-                return UnityEngine.Input.GetAxis("Mouse Y");
-            } else{
-                return 0;
+            else if (axisName == "Mouse Y")
+            {
+                if (Input.GetMouseButton(1))
+                {
+                    return -UnityEngine.Input.GetAxis("Mouse Y");
+                }
+                else
+                {
+                    return 0;
+                }
             }
+            return UnityEngine.Input.GetAxis(axisName);
         }
-        return UnityEngine.Input.GetAxis(axisName);
+        else
+        {
+            if(axisName == "Mouse X")
+            {
+                if (Input.GetMouseButton(1))
+                {
+                    return UnityEngine.Input.GetAxis("Mouse X");
+                } 
+                else
+                {
+                    return 0;
+                }
+            }
+            else if (axisName == "Mouse Y")
+            {
+                if (Input.GetMouseButton(1))
+                {
+                    return UnityEngine.Input.GetAxis("Mouse Y");
+                } 
+                else
+                {
+                    return 0;
+                }
+            }
+            return UnityEngine.Input.GetAxis(axisName);
+        }
+       
     }
 }
